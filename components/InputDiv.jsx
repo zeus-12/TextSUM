778dcb074b8d4e3138739f44aa33c1f46dbd9072
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-11-02 23:43:08 +0530

    Added loadin stage, more styling, error message:

--- components/InputDiv.jsx
+++ components/InputDiv.jsx
@@ -4,8 +4,11 @@ import { useState } from "react";
 const InputDiv = () => {
   const [data, setData] = useState("");
   const [output, setOutput] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const requestAPI = async () => {
+    setLoading(true);
     const res = await fetch("/api/gettext", {
       method: "POST",
       headers: {
@@ -14,7 +17,10 @@ const InputDiv = () => {
       body: JSON.stringify({ text: data }),
     });
     const result = await res.json();
+    setLoading(false);
     if (result.error) {
+      setError("Something went wrong, try again later!");
+
       return;
     }
     setOutput(result.intent);
@@ -38,6 +44,7 @@ const InputDiv = () => {
       <Button
         variant="outline"
         onClick={requestAPI}
+        loading={loading}
         className="hover:bg-blue-400 border-blue-400 hover:text-white"
       >
         Convert!
@@ -46,6 +53,7 @@ const InputDiv = () => {
         <p>
           <span className="text-gray-600">Result: </span>
           {output}
+          {<p className="text-red-500">{error}</p>}
         </p>
       </div>
     </div>
